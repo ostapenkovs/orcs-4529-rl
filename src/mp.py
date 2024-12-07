caa648d4d91ec13efff44372d7062f94c4485254
Vasiliy Ostapenko <ostapenkovs@pm.me>
2024-12-07 14:55:30 -0500

    params

--- src/mp.py
+++ src/mp.py
@@ -1,4 +1,5 @@
 import sys, os
+import json
 import pickle
 import time
 import numpy as np
@@ -6,7 +7,7 @@ from numpy import log, exp, sqrt
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('Agg')  
+matplotlib.use('Agg')
 
 from functools import partial
 import multiprocessing as mp
@@ -34,7 +35,7 @@ def read_data(data_dir='../data/test_cases.csv'):
 
     return df[cols + ['path_kwargs', 'gbm']].values
 
-def onesim(*args, nsim, nstep, t1, k):
+def onesim(*args, nsim, nstep, t1, k, nepisode_train, nepisode_eval):
     s_0, t2, q, r, h, path_kwargs, gbm = args
 
     env = Environment(
@@ -47,15 +48,25 @@ def onesim(*args, nsim, nstep, t1, k):
         buffer_interval=8, model_interval=32, gamma=0.995, eps=0.99, eps_decay=0.995, eps_min=0.01
     )
 
-    losses, rewards, fig1 = agent.train(nepisode=3000, notebook=False, verbose=False)
-    mean_reward, fig2 = agent.eval(nepisode=500, notebook=False)
+    losses, rewards, fig1 = agent.train(nepisode=nepisode_train, notebook=False, verbose=False)
+    mean_reward, fig2 = agent.eval(nepisode=nepisode_eval, notebook=False)
 
     return mean_reward, fig1, fig2
 
 def main():
     ### DATA ###
+    with open(f'..data/params.json', 'r') as f:
+        params = json.load(f)
+    
+    nsim = params.get('nsim', 10000)
+    nepisode_train = params.get('nepisode_train', 3000)
+    nepisode_eval = params.get('nepisode_eval', 500)
+    nstep = params.get('nstep', 365)
+    t1 = params.get('t1', 0)
+    k = params.get('k', 100)
+
     arr = read_data(data_dir='../data/test_cases.csv')
-    arr = arr[:30]
+    arr = arr[:30] # TODO: remove!
     print('Need to run:', arr.shape[0], 'simulations.')
     print()
     ### DATA ###
@@ -68,7 +79,9 @@ def main():
     start = time.time()
     
     pool = mp.Pool(processes=num_workers)
-    myfunc = partial(onesim, nsim=10000, nstep=365, t1=0, k=100)
+    myfunc = partial(
+        onesim, nsim=nsim, nstep=nstep, t1=t1, k=k, nepisode_train=nepisode_train, nepisode_eval=nepisode_eval
+    )
     results = pool.starmap(myfunc, arr)
     pool.close()
     pool.join()
